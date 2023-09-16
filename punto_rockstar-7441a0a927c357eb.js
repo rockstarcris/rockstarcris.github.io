4e034d959a193ca53f1dc6f4c8bc68097b47a2b4
Simon <simon.cheng.wd@protonmail.com>
2023-09-15 21:25:16 -0300

    Updates

--- punto_rockstar-7441a0a927c357eb.js
+++ punto_rockstar-7441a0a927c357eb.js
@@ -1028,7 +1028,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('punto_rockstar-7f28b714538dbf13_bg.wasm', import.meta.url);
+        input = new URL('punto_rockstar-7441a0a927c357eb_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
